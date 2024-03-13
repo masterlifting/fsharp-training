d9f08d48532e55cf4607a66660a572278a1635b5
Andrei Pestunov <andrei.pestunov@akvelon.com>
2024-03-13 11:54:51 +0100

    Add type definitions and functions for data transformation

--- Program.fs
+++ Program.fs
@@ -1,9 +1,33 @@
 ﻿open GoogleCloudProvider
 open System
 
+type a = { Id: string }
+
+type b = { Id: string }
+
+type du =
+    | A of a
+    | B of b
+
 [<EntryPoint>]
 let main _ =
 
+    let f1 (data: a seq) : du seq = data |> Seq.map A
+
+    let f2 (data: du seq) : a seq =
+        data
+        |> Seq.choose (function
+            | A a -> Some a
+            | _ -> None)
+
+    let f2 (data: du seq) : a seq =
+        data
+        |> Seq.choose (fun x ->
+            match x with
+            | A a -> Some a
+            | _ -> None)
+
     Async.RunSynchronously <| downloadFilesTo Environment.CurrentDirectory
+
 
     0
